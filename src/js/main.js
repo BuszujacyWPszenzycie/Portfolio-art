01de725dfbb17f09167921bd08dd9ba2df84088e
Marcin <marcin.szturminski@gmail.com>
2023-05-06 18:36:03 +0200

    added shadow on nav top, added closing nav_modal when clicking on link

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,30 @@ const body = document.querySelector('body')
 const hamburgerBtn = document.querySelector('.hamburger')
 const navModal = document.querySelector('.nav__mobile-modal')
 const navMobileIcon = document.querySelector('.nav__mobile-icon-a')
+const navMobileTop = document.querySelector('.nav__top')
+const allMobileNavItems = document.querySelectorAll('.nav__mobile-modal-item')
+console.log(allMobileNavItems)
+
+document.addEventListener('DOMContentLoaded', function () {
+	function addShadow() {
+		if (window.scrollY >= 80) {
+			navMobileTop.classList.add('nav__top-shadow')
+		} else {
+			navMobileTop.classList.remove('nav__top-shadow')
+		}
+	}
+
+	allMobileNavItems.forEach(item =>
+		item.addEventListener('click', () => {
+			body.classList.remove('stop-scrolling')
+			hamburgerBtn.classList.remove('is-active')
+			navModal.classList.remove('nav__mobile-modal-active')
+			navMobileIcon.classList.remove('nav__mobile-icon-a-active')
+		})
+	)
+
+	window.addEventListener('scroll', addShadow)
+})
 
 const openMobileNav = () => {
 	body.classList.toggle('stop-scrolling')
